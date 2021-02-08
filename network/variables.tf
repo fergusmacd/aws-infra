f8a54fc0ac11c454dddfa7d1929246ced39dbac4
Fergus MacDermot <fergusmacdermot@gmail.com>
2021-02-08 11:46:27 +0800

    Updating variables

--- network/variables.tf
+++ network/variables.tf
@@ -1,17 +1,25 @@
 #### These variables are read from the incoming from the terraform call inside terraform-wrapper.sh
 ## Project and environment values
 variable "project_name" {
+  description = "A description of what the project will do"
+  default = "Photos server - to display my photos!"
 }
 
 variable "region" {
+  description = "The aws region we run in."
+  default = "us-west-2"
 }
 
 variable "environment" {
+  description = "The environment we are running in"
+  default = "demo"
 }
 
 ### Module specific information
 ## VPC
 variable "vpc_cidr" {
+  description = ""
+  default = "10.14.0.0/23"
 }
 
 ## public_subnet_bastion
